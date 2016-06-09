c62909b9bd2dbd5cc82d58316ab1e8a5cb2b2d06
withanage <dulip.withanage@gmail.com>
2016-06-09 14:19:46 +0200

    Added module documentation for post process

--- static/tools/postProcess.py
+++ static/tools/postProcess.py
@@ -34,35 +34,14 @@ import sys
 import time
 import uuid
 import zipfile
+try:
+    from lxml import etree
+except ImportError:
+    print("Failed to import ElementTree, plase install")
+
 
 LOG_FILE = 'jatsPostProcess.log'
 logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
-
-try:
-    from lxml import etree
-
-except ImportError:
-    try:
-        # Python 2.5
-        import xml.etree.cElementTree as etree
-
-    except ImportError:
-        try:
-            # Python 2.5
-            import xml.etree.ElementTree as etree
-
-        except ImportError:
-            try:
-                # normal cElementTree install
-                import cElementTree as etree
-
-            except ImportError:
-                try:
-                    # normal ElementTree install
-                    import elementtree.ElementTree as etree
-
-                except ImportError:
-                    print("Failed to import ElementTree from any known place")
 
 
 class PostProcess:
@@ -80,49 +59,54 @@ class PostProcess:
 
         self.JATS_XML_HEADER = '<!DOCTYPE article PUBLIC "-//NLM//DTD Journal Publishing DTD v3.0 20080202//EN" "http://dtd.nlm.nih.gov/publishing/3.0/journalpublishing3.dtd"><article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" dtd-version="1.1d1">'
 
-    def get_ref_ids_back(self, tree):
-        back_refs = Set()
-        for back in tree.getroot().findall(".//back/ref-list"):
-            for ref in back.findall(".ref"):
-                if ref.keys():
-                    back_refs.add(ref.attrib['id'])
-        return back_refs
+  
+    def apply_transformations(self, tr, context):
+        ''' main method to apply transformations'''
+        tr = self.remove_not_used_in_back(tr, "ref-list/ref")
+        tr = self.remove_not_used_in_back(tr, "fn-group/fn")
+        tr = self.set_enumeration(tr, "xref", "ref-type", "fn")
+        tr = self.remove_name_duplicates_speech(tr)
+        tr = self.set_numbering(tr, ['speech', 'disp-quote'])
+        tr = self.get_unreferenced_footnotes(tr)
+        tr = self.set_uuids(tr, 'fn')
 
-    def get_footnotes_back(self, tree):
+        #
+        if "references" in self.config["createFull"][context].keys():
+            if "duplicates" in self.config[
+                    "createFull"][context]["references"].keys():
+                for i in self.config["createFull"][
+                        context]["references"]["duplicates"]:
+                    tr = self.remove_duplicate_refs(tr, i)
+        return tr
 
-        back_fns = Set()
-        for back in tree.getroot().findall(".//back/fn-group"):
-            for fn in back.findall(".fn"):
-                if fn.keys():
-                    back_fns.add(fn.attrib['id'])
-        return back_fns
+    def contains(self, a, v):
+        ''' help method to compare references '''
+        (c1, c2, c3, c4) = v
+        if c1 and c2:
+            for (v1, v2, v3, v4) in a:
+                if v1 == c1 and v2 == c2 and v3 == c3 and v4 == c4:
+                    return True
+        return False
 
-    def get_refs_mixed_back(self, tree):
-        back_refs = Set()
-        for back in tree.getroot().findall(".//back/ref-list/ref"):
-            for ref in back.findall(".//mixed-citation"):
-                loggin.info(
-                    "Mixed reference found", etree.tostring(
-                        back, pretty_print=True))
-        return back_refs
+    def create_full(self, context):
+        ''' main method : creates zip, merges and transforms xml files '''
+        cfb = self.config['createFull'][context]
+        self.create_zip(cfb["dir"], cfb["files"])
 
-    def get_ref_ids_body(self, tree):
-        body_refs = Set()
-        for body in tree.getroot().findall(".//body"):
-            for ref in body.findall(".//xref[@ref-type='bibr']"):
-                if ref.keys():
-                    body_refs.add(ref.attrib['rid'])
-        return body_refs
+        fp = os.path.join(cfb["dir"], cfb["fullfile"])
 
-    def get_footnotes_body(self, tree):
-        body_fns = Set()
-        for body in tree.getroot().findall(".//body"):
-            for fn in body.findall(".//xref[@ref-type='fn']"):
-                if fn.keys():
-                    body_fns.add(fn.attrib['rid'])
-        return body_fns
+        f = open(fp, 'w')
+        f.write(self.merge_files(cfb["dir"], cfb["files"], context))
+        f.close()
+
+        tr = self.apply_transformations(self.get_etree(fp), context)
+        self.create_output(tr, fp)
+        logging.info('outout written' + cfb["fullfile"])
+
+        return None
 
     def create_output(self, tree, file):
+        ''' write element tree to file '''
         try:
             tree.write(
                 file,
@@ -133,92 +117,109 @@ class PostProcess:
         except:
             logging.info(self.config['errors']['FILE_NOT_WRITTEN'])
 
-    def remove_not_used_in_back(self, tree, tag):
-        body_refs = self.get_ref_ids_body(tree)
-        back_refs = self.get_ref_ids_back(tree)
+    def create_refs_csv(self, dir, infile, outfile, types):
+        ''' write a csv file from the references '''
+        file = os.path.join(dir, infile)
+        tree = self.get_etree(file)
+        elem = tree.find('./back/ref-list')
+        out = ''
+        for e in elem:
+            for t in types:
+                if e.find('.//' + t) is not None:
+                    out += e.find('.//' + t).text + ','
+                out += '\n'
+        tf = open(outfile, "w")
+        tf.write(unicode(out).encode("utf-8"))
+        tf.close()
 
-        for i in back_refs:
-            if i in body_refs:
-                pass
-            else:
-                elems = tree.getroot().findall(
-                    ''.join(['.//back/', tag, '/[@id="', str(i), '"]']))
-                for e in elems:
-                    if e.getparent() is not None:
-                        e.getparent().remove(e)
-                        logging.info(tag + etree.tostring(e) + " removed")
+    def create_zip(self, dir, files):
+        ''' create a zip file from a set of files and a directory path '''
+        try:
+            z = zipfile.ZipFile(os.path.join(dir, ''.join(
+                [self.config["zip"]["name-prefix"], str(time.time()), '.zip'])), "w")
+            for f in files:
+                z.write(os.path.join(dir, f),
+                        compress_type=zipfile.ZIP_DEFLATED)
+        except:
+            pass
+
+    def get_etree(self, infile):
+        ''' reads a file and get the element tree '''
+        try:
+            tree = etree.parse(infile)
+        except:
+            print self.config['errors']['FILE_NOT_FOUND']
+            sys.exit(2)
         return tree
 
-    def get_unreferenced_footnotes(self, tree):
-        body_fns = self.get_footnotes_body(tree)
-        back_fns = self.get_footnotes_back(tree)
+    def get_footnotes_back(self, tree):
+        '''returns footnotes in back '''
+        back_fns = Set()
+        for back in tree.getroot().findall(".//back/fn-group"):
+            for fn in back.findall(".fn"):
+                if fn.keys():
+                    back_fns.add(fn.attrib['id'])
+        return back_fns
+
+    def get_footnotes_body(self, tree):
+        '''returns footnotes in body '''
+        body_fns = Set()
+        for body in tree.getroot().findall(".//body"):
+            for fn in body.findall(".//xref[@ref-type='fn']"):
+                if fn.keys():
+                    body_fns.add(fn.attrib['rid'])
+        return body_fns
+
+    def get_ref_ids_back(self, tree):
+        '''returns references in back '''
+        back_refs = Set()
+        for back in tree.getroot().findall(".//back/ref-list"):
+            for ref in back.findall(".ref"):
+                if ref.keys():
+                    back_refs.add(ref.attrib['id'])
+        return back_refs
+
+    def get_ref_ids_body(self, tree):
+        '''returns references in body '''
+        body_refs = Set()
+        for body in tree.getroot().findall(".//body"):
+            for ref in body.findall(".//xref[@ref-type='bibr']"):
+                if ref.keys():
+                    body_refs.add(ref.attrib['rid'])
+        return body_refs
+
+    def get_refs_mixed_back(self, tree):
+        '''returns mixed citations in back '''
+        back_refs = Set()
+        for back in tree.getroot().findall(".//back/ref-list/ref"):
+            for ref in back.findall(".//mixed-citation"):
+                loggin.info(
+                    "Mixed reference found", etree.tostring(
+                        back, pretty_print=True))
+        return back_refs
+    
+    def get_unreferenced_footnotes(self, tr):
+        ''' returns a list of footnotes, which are not in body '''
+        body_fns = self.get_footnotes_body(tr)
+        back_fns = self.get_footnotes_back(tr)
         for i in back_fns:
             if i in body_fns:
                 pass
             else:
                 logging.info("Footnote " + i + " is not used")
 
-        return tree
-
-    def set_enumeration(self, tree, name, attr, value):
-        searchTag = './/' + name + '[@' + attr + '="' + value + '"]'
-        elems = tree.getroot().findall(searchTag)
-        count = 1
-        for elem in elems:
-            elem.text = str(count)
-            count += 1
-        return tree
-
-    def remove_name_duplicates_speech(self, tree):
-        elems = tree.getroot().findall('.//speech/p')
-        for elem in elems:
-            for l in list(elem):
-                if l is not None:
-                    elem.text = ""
-        return tree
-
-    def apply_transformations(self, tr, book):
-        tr = self.remove_not_used_in_back(tr, "ref-list/ref")
-        tr = self.remove_not_used_in_back(tr, "fn-group/fn")
-        tr = self.set_enumeration(tr, "xref", "ref-type", "fn")
-        tr = self.remove_name_duplicates_speech(tr)
-        tr = self.set_ids_to_tag(tr, ['speech', 'disp-quote'])
-        tr = self.get_unreferenced_footnotes(tr)
-        tr = self.set_uuids(tr, 'fn')
-
-        if "references" in self.config["createFull"][book].keys():
-            if "duplicates" in self.config[
-                    "createFull"][book]["references"].keys():
-                for i in self.config["createFull"][
-                        book]["references"]["duplicates"]:
-                    tr = self.remove_duplicate_refs(tr, i)
         return tr
-
-    def set_uuids(self, tr, fn):
-        f = {}
-        fns = tr.getroot().findall(''.join(['.//xref/[@ref-type="', fn, '"]']))
-        for i in fns:
-            rid = ''.join(['bibd', uuid.uuid4().get_hex()])
-            f[i.attrib['rid']] = rid
-            i.set('rid', rid)
-        for m in f.keys():
-            n = tr.getroot().find(''.join(['.//fn/[@id="', m, '"]']))
-            if len(n) > 0:
-                n.set('id', f[m])
-            else:
-                logging.error('Element not fuounf \t' + m)
-        return tr
-
-    def merge_files(self, dir, files, book):
+    def merge_files(self, dir, files, context):
+        '''  merge  a set of xml files in a directory'''
         back_fns, body_secs, back_refs = [], [], []
         header = False
 
         for f in files:
             logging.info("Parsing file " + f)
-            tree = etree.parse(os.path.join(dir, f))
-            tree = self.apply_transformations(tree, book)
-            self.create_output(tree, os.path.join(dir, f))
-            root = tree.getroot()
+            tr = etree.parse(os.path.join(dir, f))
+            tr = self.apply_transformations(tr, context)
+            self.create_output(tr, os.path.join(dir, f))
+            root = tr.getroot()
             if not header:
                 for header_text in root.findall(".//front"):
                     header_text = ''.join(etree.tostring(
@@ -239,84 +240,23 @@ class PostProcess:
 
         return out
 
-    def create_zip(self, dir, files):
-      try:
-        z = zipfile.ZipFile(os.path.join(dir, ''.join(
-            [self.config["zip"]["name-prefix"], str(time.time()), '.zip'])), "w")
-        for f in files:
-            z.write(os.path.join(dir, f),
-                    compress_type=zipfile.ZIP_DEFLATED)
-      except:
-        pass
-
-    def create_full(self, book):
-        cfb = self.config['createFull'][book]
-        self.create_zip(cfb["dir"], cfb["files"])
-
-        fp = os.path.join(cfb["dir"], cfb["fullfile"])
-
-        f = open(fp, 'w')
-        f.write(self.merge_files(cfb["dir"], cfb["files"], book))
-        f.close()
-
-        tr = self.apply_transformations(self.get_etree(fp), book)
-        self.create_output(tr, fp)
-        logging.info('outout written' + cfb["fullfile"])
-
-        return None
-
-    def remove_table_references(self, tree, name, attr, value):
-        searchTag = './/' + name + '[@' + attr + '="' + value + '"]'
-        try:
-            with open(self.TABLESTYLEFILE, 'r') as file:
-                xslt = file.read()
-        except:
-            logging.error(self.config['errors']['FILE_NOT_FOUND'])
-            sys.exit(2)
-        xslt_doc = etree.parse(io.BytesIO(xslt))
-        transform = etree.XSLT(xslt_doc)
-        return transform(tree)
-
+    def remove_all_elements_of_type(self, tree, names):
+        ''' removes all the elements of a certain type in a element tree '''
+        for name in names:
+            for elem in tree.getroot().findall('.//' + name):
+                elem.getparent().remove(elem)
+        return tree
+    
     def remove_comments(self, tree):
+        ''' removes all the comments  in a element tree '''
         comments = tree.xpath('//comment()')
         for c in comments:
             p = c.getparent()
             p.remove(c)
         return tree
 
-    def remove_all_elements_of_type(self, tree, names):
-        for name in names:
-            for elem in tree.getroot().findall('.//' + name):
-                elem.getparent().remove(elem)
-        return tree
-
-    def get_etree(self, infile):
-        try:
-            tree = etree.parse(infile)
-        except:
-            print self.config['errors']['FILE_NOT_FOUND']
-            sys.exit(2)
-        return tree
-
-    def contains(self, a, v):
-        (c1, c2, c3, c4) = v
-        # c1 and c2 are mandatory
-        if c1 and c2:
-            for (v1, v2, v3, v4) in a:
-                if v1 == c1 and v2 == c2 and v3 == c3 and v4 == c4:
-                    return True
-        return False
-
-    def set_ids_to_tag(self, tree, tags):
-        for tag in tags:
-            sh = tree.findall('.//' + tag)
-            sid = 1
-            for i in sh:
-                i.set('id', tag.replace('-', '') + str(sid))
-                sid += 1
-        return tree
-
     def remove_duplicate_refs(self, tr, tag_list):
+        ''' removes duplicate references'''
         ref_list = tr.find('./back/ref-list')
         data = []
         drf = {}
@@ -350,7 +290,84 @@ class PostProcess:
                 ref.getparent().remove(ref)
         return tr
 
-    def sort_by_tag(self, tr, parent, tag_list):
+    
+    def remove_name_duplicates_speech(self, tr):
+        ''' removes name duplication  in  speech tag '''
+        elems = tr.getroot().findall('.//speech/p')
+        for elem in elems:
+            for l in list(elem):
+                if l is not None:
+                    elem.text = ""
+        return tr
+    def remove_not_used_in_back(self, tr, tag):
+        ''' removes  footnotes, references, which are not linked. '''
+        body_refs = self.get_ref_ids_body(tr)
+        back_refs = self.get_ref_ids_back(tr)
+
+        for i in back_refs:
+            if i in body_refs:
+                pass
+            else:
+                elems = tr.getroot().findall(
+                    ''.join(['.//back/', tag, '/[@id="', str(i), '"]']))
+                for e in elems:
+                    if e.getparent() is not None:
+                        e.getparent().remove(e)
+                        logging.info(tag + etr.tostring(e) + " removed")
+        return tr
+
+    
+    def remove_table_references(self, tree, name, attr, value):
+        searchTag = './/' + name + '[@' + attr + '="' + value + '"]'
+        try:
+            with open(self.TABLESTYLEFILE, 'r') as file:
+                xslt = file.read()
+        except:
+            logging.error(self.config['errors']['FILE_NOT_FOUND'])
+            sys.exit(2)
+        xslt_doc = etree.parse(io.BytesIO(xslt))
+        transform = etree.XSLT(xslt_doc)
+        return transform(tree)
+    def set_enumeration(self, tr, name, attr, value):
+        ''' set the count id for an attribute in a tag type '''
+        searchTag = './/' + name + '[@' + attr + '="' + value + '"]'
+        elems = tr.getroot().findall(searchTag)
+        count = 1
+        for elem in elems:
+            elem.text = str(count)
+            count += 1
+        return tr
+      
+    def set_numbering(self, tree, tags):
+        ''' automatic numbering of certain tags '''
+        for tag in tags:
+            sh = tree.findall('.//' + tag)
+            sid = 1
+            for i in sh:
+                i.set('id', tag.replace('-', '') + str(sid))
+                sid += 1
+        return tree
+
+    
+    def set_uuids(self, tr, str):
+        ''' removes name confilcits for references or footnotes'''
+        f = {}
+        fns = tr.getroot().findall(
+            ''.join(['.//xref/[@ref-type="', str, '"]']))
+        for i in fns:
+            rid = ''.join(['bibd', uuid.uuid4().get_hex()])
+            f[i.attrib['rid']] = rid
+            i.set('rid', rid)
+        for m in f.keys():
+            n = tr.getroot().find(''.join(['.//fn/[@id="', m, '"]']))
+            if len(n) > 0:
+                n.set('id', f[m])
+            else:
+                logging.error('Element not found \t' + m)
+        return tr
+
+    def sort_references(self, tr, parent, tag_list):
+        ''' sort all the references  '''
         elem = tr.find('./back/ref-list')
         data = []
         for e in elem:
@@ -364,25 +381,11 @@ class PostProcess:
 
         return tr
 
-    def create_csv_from_refs(self, dir, infile, outfile, types):
-        file = os.path.join(dir, infile)
-        tree = self.get_etree(file)
-        elem = tree.find('./back/ref-list')
-        out = ''
-        for e in elem:
-            for t in types:
-                if e.find('.//' + t) is not None:
-                    out += e.find('.//' + t).text + ','
-                out += '\n'
-        text_file = open(outfile, "w")
-        text_file.write(unicode(out).encode("utf-8"))
-        text_file.close()
-
 
 def main():
     p = PostProcess('postProcessConfig.json')
-    book = "kemp"
-    p.create_full(book)
+    context = "kemp"
+    p.create_full(context)
     with open(LOG_FILE) as f:
         print f.read()
     os.remove(LOG_FILE)
