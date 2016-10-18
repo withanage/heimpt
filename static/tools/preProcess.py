c8efa69951d3432048eb4b5ef5eb9afac1a76e89
withanage <dulip.withanage@gmail.com>
2016-10-18 17:12:45 +0200

    preProcess.py

--- static/tools/preProcess.py
+++ static/tools/preProcess.py
@@ -1,50 +1,73 @@
 # -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
-
-# <codecell>
-
 # Copyright 13-October-2016, 14:51:07
 #
 # Author    : Dulip Withanage , University of Heidelberg
 
 
-import json, sys
-from debug import Debuggable
+import os, json, sys
+from debug import Debuggable, Debug
 from docopt import docopt
-
+from subprocess import Popen, PIPE
+from attrdict import AttrDict
 
 class PreProcess(Debuggable):
-    """ converts  source files into xml using the typsetter module"""
+    """ converts  source files into xml using the typesetter module"""
     
     def __init__(self):
         self.args = self.read_command_line()
         self.debug = Debug()
         Debuggable.__init__(self, 'Main')
         
-        if self.args['--debug']:
+        if self.args.get('--debug'):
             self.debug.enable_debug()
-        
-        if self.args['--config']:
-            with open(self.args['--config']) as j:
-                self.config = json.load(j)
-        else:
-             self.debug.print_debug(self, u'Metadata file wasn\'t specified')
-             sys.exit(1)
-        
-        
+            
+    def run(self):
+        self.config = self.read_json(self.args['<config_file>'])
+        self.typeset()
+        return        
     
     @staticmethod
     def read_command_line():
-        doc="""Usage: preProcess.py [CONFIG] 
-            preProcess.py (--doctest | --testsuite=DIR)
+        doc="""
+        Usage: 
+            preProcess.py  <config_file> [options] 
+            preProcess.py (-h | --help | --version)
+        Options:
+            -h, --help  Show this screen and exit.
+            -d, --debug Enable debug
             """ 
         return docopt(doc, version='mpt 0.1')
     
-    def run(self):
-        return
-            
+    @staticmethod
+    def fatal_error(module, message):
+        print(u'[FATAL ERROR] [{0}] {1}'.format(module.get_module_name(), message))
+        sys.exit(1)
+
+    
+    def read_json(self, f):
+        if os.path.isfile(f): 
+            with open(f) as j:
+                return AttrDict(json.load(j))
+        else:
+            self.debug.print_debug(self, u'Metadata file wasn\'t specified')
+            sys.exit(1)    
+
+    def typeset(self):
+        print self.config
+        mt = ["meTypeset/bin/meTypeset.py","docx", "sdsd", "aadsa"]
+        if  self.config.get('typesetter'):
+            for  e in self.config.typesetter:
+                if e == 'metadata':
+                    print  mt.append('--'+e+"="+self.config.typesetter.metadata)
+                else:
+                    mt.append('--'+e) 
+        process = Popen(["python", ' '.join(mt)], stdout=PIPE)
+        (output, err) = process.communicate()
+        exit_code = process.wait()
+        return (output, err) , exit_code
         
 def main():
+    print 10*'-'
     pre_process_instance = PreProcess()
     pre_process_instance.run()
 
@@ -52,8 +75,4 @@ def main():
 if __name__ == '__main__':
     main()
       
-        
-
-# <codecell>
-
 
