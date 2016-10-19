84111c4cf1c6a2b5f2464c65e5c7d346a3a73c19
withanage <dulip.withanage@gmail.com>
2016-10-19 10:39:16 +0200

    preProcess.py

--- static/tools/debug.py
+++ static/tools/debug.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
-
-# <codecell>
-
 # Copyright 13-October-2016, 14:51:07
 #
 # Author    : Dulip Withanage , University of Heidelberg
@@ -10,6 +6,19 @@ class Debuggable(object):
     def __init__(self, module_name):
         self.module_name = module_name
 
-# <codecell>
+class Debug(object):
+    def __init__(self):
+        '''   Initialise this debug instance '''
+        self.debug = False
+    
+    def enable_debug(self):
+        self.debug = True
+        
+    def print_debug(self, module, message):
+        ''' Print debug message ''' 
+        if self.debug:
+            if not isinstance(message, unicode):
+                self.fatal_error(self, u'A non unicode string was passed to the debugger')
+            self.print_(module, message)
 
-
+        
