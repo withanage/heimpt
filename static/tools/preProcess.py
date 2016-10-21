fddfcacd9a8b59e39aa782c0d447fd1fb54e6e68
withanage <dulip.withanage@gmail.com>
2016-10-21 10:00:02 +0200

    preProcess.py

--- static/tools/preProcess.py
+++ static/tools/preProcess.py
@@ -13,6 +13,7 @@ import collections
 import json
 import sys
 import uuid
+from globals import GV 
 from debug import Debuggable, Debug
 from docopt import docopt
 from subprocess import Popen, PIPE
@@ -26,6 +27,7 @@ class PreProcess(Debuggable):
     def __init__(self):
         self.args = self.read_command_line()
         self.debug = Debug()
+        self.gv= GV()
         Debuggable.__init__(self, 'Main')
         if self.args.get('--debug'):
             self.debug.enable_debug()
@@ -60,7 +62,7 @@ class PreProcess(Debuggable):
             with open(f) as j:
                 return json.load(j)
         else:
-            self.debug.print_debug(self, u'Metadata file wasn\'t specified')
+            self.debug.print_debug(self,)
             sys.exit(1)
 
     def typeset_run(self, mt):
@@ -75,7 +77,7 @@ class PreProcess(Debuggable):
         if project.get('path'):
             self.typeset_run(mt)
         else:
-            self.debug.print_debug(self, u'project folder not specified')
+            self.debug.print_debug(self, self.gv.TYPESETTER_METADATA_FILE_WAS_NOT_SPECIFIED)
 
     def typeset_files(self, project, ct):
 
@@ -83,25 +85,24 @@ class PreProcess(Debuggable):
         od_fs = collections.OrderedDict(sorted(fs.items()))
         for f in od_fs:
             mt = [ct.get('language')] if ct.get('language') else self.debug.print_debug(
-                self, u'typesetter language is not specified')
+                self,self.gv.TYPESETTER_IS_NOT_SPECIFIED)
             if os.path.isfile(ct.get('path')):
                 mt.append(
                     ct.get('path')) if ct.get('path') else self.debug.print_debug(
-                    self, u'typesetter path is not specified')
+                    self,self.gv.TYPESETTER_PATH_IS_NOT_SPECIFIED )
                 mt.append(
                     project.get("type")) if project.get("type") else self.debug.print_debug(
-                    self, u'typesetter type is not specified')
-                #mt.append(project.get("output")) if project.get("output") else self.debug.print_debug(self, u'typesetter type is not specified')
+                    self, self.gv.INPUT_FILE_TYPE_IS_NOT_SPECIFIED )
                 fl = os.path.join(project.get('path'), od_fs[f])
                 if  os.path.isfile(fl):
                     mt.append(fl)
                     mt.append(os.path.join(project.get('path'), str(uuid.uuid4())))
                     self.typeset_file(project, mt, od_fs, f)
                 else:
-                    self.debug.print_debug(self, u'input_file does not exist: '+od_fs[f].encode('utf-8'))
+                    self.debug.print_debug(self, self.gv.INPUT_FILE_DOES_NOT_EXIST+od_fs[f].encode('utf-8'))
             else:
                 self.debug.print_debug(
-                    self, u'typesetter binary is not innstalled')
+                    self,self.gv.TYPESETTER_BINARY_IS_UNAVAILABLE)
 
     def tpyeset_project(self, project):
         ''' runs typesetter for  a project '''
@@ -137,6 +138,7 @@ class PreProcess(Debuggable):
 def main():
     pre_process_instance = PreProcess()
     pre_process_instance.run()
+    print '-'*50
 
 
 if __name__ == '__main__':
