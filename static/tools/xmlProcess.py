aed552564f5d1e778c384f6e3e1e64fa5ff6ed4c
withanage <dulip.withanage@gmail.com>
2016-10-24 14:00:01 +0200

    Dev version

--- static/tools/xmlProcess.py
+++ static/tools/xmlProcess.py
@@ -7,11 +7,12 @@ Options:
     -d, --debug                                     Enable debug output
 """
 
+
 __author__ = "Dulip Withnage"
 
 PYTHON_IMPORT_FAILED_LXML_MODULE= u'Failed to import python lxml module'
 
-import sys , globals
+import sys 
 from debug import Debuggable, Debug
 from docopt import docopt
 from globals import GV
@@ -24,16 +25,20 @@ except ImportError:
     print(PYTHON_IMPORT_FAILED_LXML_MODULE)
     sys.exit(1)
     
-class xmlProcess(Debuggable):
+class XMLProcess(Debuggable):
     '''     command line tool to clean, modify, delete, merge jats files    '''
+    
     def __init__(self):
         self.args = self.read_command_line()
         self.gv = GV()
+    
     @staticmethod
     def read_command_line():
         return docopt(__doc__, version='xml 0.1')
     
     
-        
+def main():
+    ppo = XMLProcess()
+   
 if __name__ == '__main__':
-    pass
+    main()
