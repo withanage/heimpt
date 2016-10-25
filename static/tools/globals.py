a046b296331dcf3c58915b370b37e50e375d406d
withanage <dulip.withanage@gmail.com>
2016-10-25 16:00:01 +0200

    Dev version

--- static/tools/globals.py
+++ static/tools/globals.py
@@ -2,7 +2,7 @@
 
 __author__ = "Dulip Withnage"
 
-import json, os, sys
+import json, os, sys ,shutil
 from debug import Debuggable, Debug
 
 class GV(object):
@@ -13,10 +13,12 @@ class GV(object):
         # GLOBAL VARIABLES
         
         # projects
+        self.PROJECT_INPUT_FILE_JSON_IS_NOT_VALID       = u'project input file json is not valid'
         self.PROJECT_INPUT_FILE_TYPE_IS_NOT_SPECIFIED   = u'project input file type is not specified'
         self.PROJECT_INPUT_FILE_HAS_MORE_THAN_TWO_DOTS  = u'project input file has more than two dots'
         self.PROJECT_INPUT_FILE_DOES_NOT_EXIST          = u'project input_file does not exist'
         self.PROJECT_IS_NOT_ACTIVE                      = u'project is not active'
+        self.PROJECT_OUTPUT_FILE_WAS_NOT_CREATED        = u'sproject output file was not created'
         self.PROJECT_TYPESETTER_IS_NOT_AVAILABLE        = u'project typesetter is not available'
         self.PROJECT_TYPESETTER_IS_NOT_SPECIFIED        = u'project typesetter is not specified'
         self.PROJECT_TYPESETTER_NAME_IS_NOT_SPECIFIED   = u'project typesetter name is not specified'
@@ -32,6 +34,9 @@ class GV(object):
         self.TYPESETTER_PATH_IS_NOT_SPECIFIED           = u'typesetter path is not specified '
         self.TYPESETTER_BINARY_IS_UNAVAILABLE           = u'typesetter binary is unavailable '
         self.TYPESETTER_RUNS_WITH_NO_ARGUMENTS          = u'typesetter runs with no arguments'
+        
+        #
+        self.debug = Debug()
         
     @staticmethod
     def fatal_error(module, message):
@@ -51,26 +56,36 @@ class GV(object):
             with open(f) as j:
                 return json.load(j)
         else:
-            self.debug.print_debug(self,)
+            self.debug.print_debug(self,self.PROJECT_INPUT_FILE_JSON_IS_NOT_VALID)
             sys.exit(1)
     
+
+    def create_dirs_recursive(self, project_path):
+        p = ''
+        for path in project_path:
+            p = p + os.path.sep + path.strip('/').strip('/')
+            if not os.path.exists(p):
+                os.makedirs(p)
+        
+        return p
+
     def reorganize_output(self, ppath, project, typesetter, i, time_now, file_prefix, od_fs, f, uid):
         temp_path = [ppath, uid]
         if typesetter=='metypeset':
             temp_path = temp_path + ['nlm'] 
-            print temp_path
         out_type = project['typesetters'][i]['out_type']
         out_file = file_prefix+'.'+out_type
         temp_path.append(out_file)
-        temp_path = '/'.join(temp_path)
-        if os.path.isfile(temp_path):
+        temp_file = os.path.sep.join(temp_path)
+        if os.path.isfile(temp_file):
             project_path = [ppath, project['name'], time_now,  i+'_'+typesetter,out_type]
-            if os.path.isdir(project_path) == False:
-                os.makedirs(project_path, '0o777')
-            project_path = '/'.join([project_path, out_file])
-            print project_path
-          #os.rename(temp_path, project_path
+            p = self.create_dirs_recursive(project_path)
+            file_path = p + os.path.sep+ out_file
+            os.rename(temp_file, file_path)
+            shutil.rmtree(os.path.join(ppath, uid))
+            
         else:
+             self.debug.print_debug(self,self.PROJECT_OUTPUT_FILE_WAS_NOT_CREATED)
               
           
           
