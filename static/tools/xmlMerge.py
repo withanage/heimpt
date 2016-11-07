ad690f9b9007cb48542c3b914f3927b33df95189
withanage <dulip.withanage@gmail.com>
2016-11-07 16:01:07 +0100

    Dev version

---
+++ static/tools/xmlMerge.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Usage:
+    xmlMerge.py  <input_file>  <path>  <scheme> <full_file>  [options]
+    xmlMerge.py -h --help
+Options:
+    -d, --debug   Enable debug output
+    --header-file
+
+"""
+
+
+__author__ = "Dulip Withnage"
+
+PYTHON_IMPORT_FAILED_LXML_MODULE = u'Failed to import python lxml module'
+
+import os
+import sys
+import uuid
+from debug import Debuggable, Debug
+from docopt import docopt
+from globals import GV
+
+
+try:
+    from lxml import etree
+    from lxml import objectify
+except ImportError:
+    print(PYTHON_IMPORT_FAILED_LXML_MODULE)
+    sys.exit(1)
+
+
+class XMLProcess(Debuggable):
+    '''     command line tool to clean, modify, delete, merge jats files    '''
+
+    def __init__(self):
+        self.args = self.read_command_line()
+        print self.args
+        self.debug = Debug()
+        self.gv = GV()
+        self.uid = '4e4dd8cf-26bf-4893-b037-1fd3bf08f112'
+        self.dr = self.args.get('<path>')
+        Debuggable.__init__(self, 'Main')
+        if self.args.get('--debug'):
+            self.debug.enable_debug()
+
+    @staticmethod
+    def read_command_line():
+
+        return docopt(__doc__, version='xml 0.1')
+
+    def xml_elements_to_array(self, xpath_expression, root):
+
+        elem_array = []
+        try:
+            for i in root.findall(xpath_expression):
+                elem_array.append(etree.tostring(i, pretty_print=False))
+        except SyntaxError as se:
+            print se
+        return elem_array
+
+    def get_front(self, fl):
+
+        hf = self.args.get('--header-file')
+        if hf:
+            f = open(self.uid + 'body.xml')
+            s = f.read()
+            f.close()
+            return s
+        else:
+            return fl
+
+    def write_array_to_file(self, f, name):
+
+        t = open(os.path.join(self.dr, self.uid + '.' + name + '.xml'), 'w')
+        for i in f:
+            t.write(i)
+        t.close()
+
+    def create_temp_files(self, front, body, back):
+
+        self.write_array_to_file(front, 'front')
+        self.write_array_to_file(body, 'body')
+        self.write_array_to_file(back, 'back')
+
+    def create_output(self, tr):
+
+        root = tr.getroot()
+        front = self.xml_elements_to_array(".//front", root)
+        body = self.xml_elements_to_array(".//body", root)
+        back = self.xml_elements_to_array(".//back", root)
+
+        if front and body and back:
+            front = self.get_front(front)
+            self.create_temp_files(front, body, back)
+        else:
+            self.debug.print_debug(self, self.gv.XML_INPUT_FILE_IS_NOT_VALID)
+            sys.exit(1)
+
+        return tr
+
+    def process_xml_file(self):
+
+        f = self.args.get('<input_file>')
+        tr = etree.parse(os.path.join(self.dr, f))
+        count = 1
+        range_count = [1, 2]
+        self.gv.create_dirs_recursive(self.dr.split('/'))
+        tr = self.create_output(tr)
+        self.gv.create_xml_file(tr, os.path.join(self.dr, os.path.basename(f)))
+
+    def run(self):
+        self.process_xml_file()
+
+
+def main():
+
+    xp = XMLProcess()
+    xp.run()
+
+
+if __name__ == '__main__':
+    main()
