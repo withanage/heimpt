d02b4c429bf3f72f117c84ffcfe25ed4fbc5ac08
withanage <dulip.withanage@gmail.com>
2016-11-09 16:00:26 +0100

    Dev version

--- static/tools/mpt.py
+++ static/tools/mpt.py
@@ -49,7 +49,7 @@ class PreProcess(Debuggable):
 
     def call_typesetter(self, mt):
         m = ' '.join(mt).strip().split(' ')
-        print m
+        print  ' '.join(m)
         process = Popen(m, stdout=PIPE)
         output, err = process.communicate()
         exit_code = process.wait()
