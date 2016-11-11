2cca9aa0c367ac0c8b31658ff6cbf57bee3e2141
withanage <dulip.withanage@gmail.com>
2016-11-11 14:11:22 +0100

    Dev version

--- static/docs/.build/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Monograph Publishing Tool&#8217;s documentation!"],titleterms:{document:0,indice:0,monograph:0,publish:0,tabl:0,tool:0,welcom:0}})
