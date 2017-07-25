f4ba669d6e3422f17bb3b3454b1566e374d19a1b
withanage <dulip.withanage@gmail.com>
2017-07-25 12:17:26 +0200

    impot solr disable

--- models/db.py
+++ models/db.py
@@ -130,5 +130,5 @@ auth.settings.reset_password_requires_verification = True
 # after defining tables, uncomment below to enable auditing
 # -------------------------------------------------------------------------
 # auth.enable_record_versioning(db)
-import solr
+
 #solrz = solr.SolrConnection('http://localhost:8984/solr/')
